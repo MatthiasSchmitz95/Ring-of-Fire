88d4ff0633cf8188318a4e86fd674e0b54c0ae28
Matthias Schmitze <m6schmitz123@gmail.comm>
2023-06-23 09:55:39 +0200

    problems with collection

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Game } from 'src/modules/game';
 import { MatDialog } from '@angular/material/dialog';
 import { AddPlayerComponent } from '../add-player/add-player.component';
+import { Firestore, collection, collectionData, addDoc, getDoc, doc, getFirestore, onSnapshot } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+
+
+
 
 
 @Component({
@@ -9,42 +15,72 @@ import { AddPlayerComponent } from '../add-player/add-player.component';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
+
 export class GameComponent implements OnInit {
+  firestore: Firestore = inject(Firestore);
+  games: Observable<any[]>;
+
+
   pickedCard: boolean = false;
   currentCard: string = '';
   game !: Game;
 
-  constructor(public dialog: MatDialog) {
+
+  constructor(private route: ActivatedRoute, public dialog: MatDialog) {
+    const aCollection = collection(this.firestore, 'games');
+    this.games = collectionData(aCollection);
+
+
+
+
 
   }
 
   ngOnInit(): void {
     this.newGame();
     console.log(this.game);
+
+    this.route.params.subscribe(async(params) => {
+      console.log('params', params);
+      this.firestore
+      .collection('games')
+      .valueChange()
+      .subscribe((game) =>{
+        console.log(game)
+
+      });
+
+
+
+    }
+   );
+
   }
 
   newGame() {
     this.game = new Game();
+     //addDoc(collection(this.firestore, 'games'), this.game.toJson());
   }
 
   pickCard() {
-    if ( this.game.players.length != 0) {
-      if(!this.pickedCard){
-      this.currentCard = this.game.stack.pop();
-      console.log(this.currentCard);
-      this.pickedCard = true;
-      this.game.currentPlayer++;
-      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
-      console.log(this.game.currentPlayer);
-      setTimeout(() => {
-        this.game.playedCards.push(this.currentCard);
-        this.pickedCard = false;
-      }, 1250);
+    if (this.game.players.length != 0) {
+      if (!this.pickedCard) {
+        this.currentCard = this.game.stack.pop();
+        console.log(this.currentCard);
+        this.pickedCard = true;
+        this.game.currentPlayer++;
+        this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+        console.log(this.game.currentPlayer);
+        setTimeout(() => {
+          this.game.playedCards.push(this.currentCard);
+          this.pickedCard = false;
+        }, 1250);
 
-    }}
-    else{
+      }
+    }
+    else {
       alert("please enter a Plyer to play the Game");
-        
+
     }
 
   }
@@ -56,10 +92,10 @@ export class GameComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe(name => {
-      if (name && name.length >0) {
+      if (name && name.length > 0) {
         console.log('The dialog was closed');
         this.game.players.push(name);
-        
+
       }
 
     });
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-ae2ea',
+    appId: '1:117142117900:web:d60e4a58b7ca2f8a969829',
+    storageBucket: 'ring-of-fire-ae2ea.appspot.com',
+    apiKey: 'AIzaSyCJdu7OlglZAqSBgtuJIDukV6pK1jccAMM',
+    authDomain: 'ring-of-fire-ae2ea.firebaseapp.com',
+    messagingSenderId: '117142117900',
+  },
+};
